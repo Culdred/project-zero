2a209c939096d59ebb0a170b741f88fd7ee4b118
Culdred <elia.gentilucci@gmail.com>
2020-12-22 14:55:00 +0100

    Add PerksTree component

---
+++ src/components/PerksTree.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <Perk name="Perk1" :img-url="'https://image.shutterstock.com/image-vector/three-gear-sign-simple-icon-260nw-417078436.jpg'" max-value=2 />
+  </div>
+</template>
+
+<script>
+import Perk from './Perk.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Perk
+  }
+}
+</script>
