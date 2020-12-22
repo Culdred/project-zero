c212fc28405347b3c1d6f1b5f581678e71e446ef
Culdred <elia.gentilucci@gmail.com>
2020-12-22 16:01:42 +0100

    Update structure

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,77 @@
 <template>
   <div id="app">
-    <button v-show="!isVisible" @click="isVisible = !isVisible; tree=''">
-      Back
-    </button>
     <div v-show="isVisible">
-      <Perk name="Int" :img-url="'https://image.shutterstock.com/image-vector/three-gear-sign-simple-icon-260nw-417078436.jpg'" max-value=20 />
+      
+      <!-- Body -->
+      <Perk name="Body" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <button @click="isVisible = !isVisible; tree='Athletics'">Athletics</button>
+      <button @click="isVisible = !isVisible; tree='Annihilation'">Annihilation</button>
+      <button @click="isVisible = !isVisible; tree='Street Brawler'">Street Brawler</button>
+
+      <!-- Reflexes -->
+      <Perk name="Reflexes" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/reflexes-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <button @click="isVisible = !isVisible; tree='Blades'">Blades</button>
+      <button @click="isVisible = !isVisible; tree='Handguns'">Handguns</button>
+      <button @click="isVisible = !isVisible; tree='Assault'">Assault</button>
+
+      <!-- Technical Ability -->
+      <Perk name="Technical Ability" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/technical-ability-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <button @click="isVisible = !isVisible; tree='Crafting'">Crafting</button>
+      <button @click="isVisible = !isVisible; tree='Engineering'">Engineering</button>
+
+      <!-- Intelligence -->
+      <Perk name="Intelligence" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/intelligence-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <button @click="isVisible = !isVisible; tree='Breach'">Breach</button>
+      <button @click="isVisible = !isVisible; tree='Quickhacks'">Quickhacks</button>
+      
+      <!-- Cool -->
+      <Perk name="Cool" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/cool-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <button @click="isVisible = !isVisible; tree='Stealth'">Stealth</button>
+      <button @click="isVisible = !isVisible; tree='Cold Blood'">Cold Blood</button>
+
     </div>
-    <button @click="isVisible = !isVisible; tree='Breach'" v-show="isVisible">Breach</button>
+
+    <div v-if="tree === 'Athletics'">
+        <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Annihilation'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Street Brawler'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Blades'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Handguns'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Assault'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Crafting'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Engineering'">
+      <PerksTree :name='tree' />
+    </div>
     <div v-if="tree === 'Breach'">
       <PerksTree :name='tree' />
     </div>
-    <button @click="isVisible = !isVisible; tree='Quickhacks'" v-show="isVisible">Quickhacks</button>
     <div v-if="tree === 'Quickhacks'">
       <PerksTree :name='tree' />
     </div>
+    <div v-if="tree === 'Stealth'">
+      <PerksTree :name='tree' />
+    </div>
+    <div v-if="tree === 'Cold Blood'">
+      <PerksTree :name='tree' />
+    </div>
+
+    <button v-show="!isVisible" @click="isVisible = !isVisible; tree=''">
+      Back
+    </button>
+
   </div>
 </template>
 
