d74ee45e95ee9c655bebf3b7fb6ec3bbde3fda0a
Culdred <elia.gentilucci@gmail.com>
2020-12-22 16:32:33 +0100

    Add use of v-for

--- src/App.vue
+++ src/App.vue
@@ -2,36 +2,44 @@
   <div id="app">
     <div v-show="isVisible">
       
-      <!-- Body -->
-      <Perk name="Body" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
+      <div v-for="(stat, idx) in statList" :key="idx">
+        <Perk :name="stat.name" :img-url="stat.img" max-value=20 />
+        <div v-for="(perkTree, idx) in stat.perkTrees" :key="idx">
+          <button @click="isVisible = !isVisible; tree='perkTree'">{{ perkTree }}</button>
+        </div>
+      </div>
+
+      <!-- <Perk name="Body" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
       <button @click="isVisible = !isVisible; tree='Athletics'">Athletics</button>
       <button @click="isVisible = !isVisible; tree='Annihilation'">Annihilation</button>
       <button @click="isVisible = !isVisible; tree='Street Brawler'">Street Brawler</button>
 
-      <!-- Reflexes -->
       <Perk name="Reflexes" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/reflexes-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
       <button @click="isVisible = !isVisible; tree='Blades'">Blades</button>
       <button @click="isVisible = !isVisible; tree='Handguns'">Handguns</button>
       <button @click="isVisible = !isVisible; tree='Assault'">Assault</button>
 
-      <!-- Technical Ability -->
       <Perk name="Technical Ability" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/technical-ability-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
       <button @click="isVisible = !isVisible; tree='Crafting'">Crafting</button>
       <button @click="isVisible = !isVisible; tree='Engineering'">Engineering</button>
 
-      <!-- Intelligence -->
       <Perk name="Intelligence" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/intelligence-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
       <button @click="isVisible = !isVisible; tree='Breach'">Breach</button>
       <button @click="isVisible = !isVisible; tree='Quickhacks'">Quickhacks</button>
       
-      <!-- Cool -->
       <Perk name="Cool" :img-url="'https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/cool-attribute-cyberpunk-2077-wiki-guide.png'" max-value=20 />
       <button @click="isVisible = !isVisible; tree='Stealth'">Stealth</button>
-      <button @click="isVisible = !isVisible; tree='Cold Blood'">Cold Blood</button>
+      <button @click="isVisible = !isVisible; tree='Cold Blood'">Cold Blood</button> -->
 
     </div>
 
-    <div v-if="tree === 'Athletics'">
+    <div v-for="(perkTree, idx) in perkTreeList" :key="idx">
+      <div v-if="tree === perkTree">
+        <PerksTree :name='perkTree' />
+      </div>
+    </div>
+
+    <!-- <div v-if="tree === 'Athletics'">
         <PerksTree :name='tree' />
     </div>
     <div v-if="tree === 'Annihilation'">
@@ -66,7 +74,7 @@
     </div>
     <div v-if="tree === 'Cold Blood'">
       <PerksTree :name='tree' />
-    </div>
+    </div> -->
 
     <button v-show="!isVisible" @click="isVisible = !isVisible; tree=''">
       Back
@@ -89,6 +97,64 @@ export default {
     return {
       isVisible: true,
       tree: "",
+      perkTreeList: [
+        "Athletics",
+        "Annihilation",
+        "Street Brawler",
+        "Blades",
+        "Handguns",
+        "Assault",
+        "Crafting",
+        "Engineering",
+        "Breach",
+        "Quickhacks",
+        "Stealth",
+        "Cold Blood"
+        ],
+      statList: [
+        {
+          name: "Body",
+          img: "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png",
+          perkTrees: [
+            "Athletics",
+            "Annihilation",
+            "Street Brawler",
+          ]
+        },
+        {
+          name: "Reflexes",
+          img: "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/reflexes-attribute-cyberpunk-2077-wiki-guide.png",
+          perkTrees: [
+            "Blades",
+            "Handguns",
+            "Assault",
+          ]
+        },
+        {
+          name: "Technical Ability",
+          img: "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/technical-ability-attribute-cyberpunk-2077-wiki-guide.png",
+          perkTrees: [
+            "Crafting",
+            "Engineering",
+          ]
+        },
+        {
+          name: "Intelligence",
+          img: "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/intelligence-attribute-cyberpunk-2077-wiki-guide.png",
+          perkTrees: [
+            "Breach",
+            "Quickhacks",
+          ]
+        },
+        {
+          name: "Cool",
+          img: "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/cool-attribute-cyberpunk-2077-wiki-guide.png",
+          perkTrees: [
+            "Stealth",
+            "Cold Blood",
+          ]
+        }
+      ]
     }
   },
 }
