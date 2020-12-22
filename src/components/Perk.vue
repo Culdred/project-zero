57b1b7c74271defb5532dcd42a5bcb8597a6cff8
Culdred <elia.gentilucci@gmail.com>
2020-12-22 14:54:32 +0100

    Add Perk and Stat components

---
+++ src/components/Perk.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <button @mousedown.left="addValue()" @mousedown.right="subtractValue()" @contextmenu.prevent="">
+            <img :src="imgUrl">
+            {{ value }} / {{ maxValue }}
+            <br>
+            {{ name }}
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        name: String,
+        maxValue: Number,
+        imgUrl: String,
+    },
+    data: function() {
+        return {
+            value: 0,
+        }
+    },
+    methods: {
+        addValue() {
+            if(this.value < this.maxValue) {
+                return this.value++;
+            }
+        },
+        subtractValue() {
+            if(this.value > 0) {
+                return this.value--;
+            }
+        }
+    }
+}
+</script>
+
+<style>
+    img {
+        height: 50px;
+        width: 50px;
+    }
+</style>
