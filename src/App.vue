fd16f724727d9e4eccd840693e60c9a1285688af
Culdred <elia.gentilucci@gmail.com>
2020-12-22 14:54:50 +0100

    Add components to mail component

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,36 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <button v-show="!isVisible" @click="isVisible = !isVisible; tree=''">
+      Back
+    </button>
+    <div v-show="isVisible">
+      <Stat name="Int" :img-url="'https://image.shutterstock.com/image-vector/three-gear-sign-simple-icon-260nw-417078436.jpg'" max-value=20 />
+    </div>
+    <button @click="isVisible = !isVisible; tree='Breach'" v-show="isVisible">Breach</button>
+    <div v-if="tree === 'Breach'">
+      <Perk name="Perk1" :img-url="'https://image.shutterstock.com/image-vector/three-gear-sign-simple-icon-260nw-417078436.jpg'" max-value=2 />
+    </div>
+    <br>
+    <button @click="isVisible = !isVisible" v-show="isVisible">Quickhacks</button>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Stat from './components/Stat.vue'
+import Perk from './components/Perk.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Stat,
+    Perk,
+  },
+  data: function() {
+    return {
+      isVisible: true,
+      tree: "",
+    }
+  },
 }
 </script>
 
