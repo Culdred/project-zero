8bb7c18351df4a36d70c881f414473af4cd8ce36
Nicolò Maria Mezzopera <nicolo.mezzopera@gmail.com>
2020-12-29 15:39:17 +0100

    chore: refactor to decouple config from data

--- src/App.vue
+++ src/App.vue
@@ -17,7 +17,7 @@
           :name="stat.name"
           :img-url="stat.img"
           :max-value="20"
-          v-model="stat.value"
+          v-model="allocatedPoints[stat.name]"
         />
         <div
           v-for="(perkTree, perkTreeIdx) in stat.perkTrees"
@@ -49,7 +49,7 @@
           :type="'perk'"
           :img-url="perk.imgUrl"
           :max-value="perk.maxValue"
-          v-model="perk.value"
+          v-model="allocatedPoints[perk.name]"
         />
       </div>
     </div>
@@ -58,409 +58,50 @@
 
 <script>
 import Perk from "./components/Perk.vue";
+import {
+  coolStats,
+  intelligenceStats,
+  technicalAbilityStats,
+  reflexesStats,
+  bodyStat,
+} from "./data";
 
 export default {
   name: "App",
   components: {
     Perk,
   },
-  data: function() {
+  data() {
     return {
       isChildPage: true,
       statIndex: 0,
       perkTreeIndex: 0,
+      allocatedPoints: {},
       statList: [
-        {
-          name: "Body",
-          info: "",
-          img:
-            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png",
-          value: 0,
-          perkTrees: [
-            {
-              name: "Athletics",
-              perks: [
-                {
-                  name: "Gladiator",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/gladiator-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "Pack Mule",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/pack-mula-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Epimorphisis",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/edimorphosis-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-                {
-                  name: "Like a Butterfly",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/like-a-butterfly-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Dog of War",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/dog-of-war-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "Marathoner",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/marathoner-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Multitasker",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/multitasker-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Steel and Chrome",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/steel-and-chrome-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "Divided Attention",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/divided-attention-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Invincible",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/invincible-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-                {
-                  name: "Soft on your feet",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/soft-on-your-feet-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Annihilation",
-              perks: [
-                {
-                  name: "Pump it, louder!",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/pump-it-louder-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "Momentum Shift",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/momentum-shift-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Street Brawler",
-              perks: [
-                {
-                  name: "Crushing Blows",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/crushing-blows-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-                {
-                  name: "Opportune Strike",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/opportune-strike-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Reflexes",
-          info: "",
-          img:
-            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/reflexes-attribute-cyberpunk-2077-wiki-guide.png",
-          value: 0,
-          perkTrees: [
-            {
-              name: "Blades",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Handguns",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Assault",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Technical Ability",
-          info: "",
-          img:
-            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/technical-ability-attribute-cyberpunk-2077-wiki-guide.png",
-          value: 0,
-          perkTrees: [
-            {
-              name: "Crafting",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Engineering",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Intelligence",
-          info: "",
-          img:
-            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/intelligence-attribute-cyberpunk-2077-wiki-guide.png",
-          value: 0,
-          perkTrees: [
-            {
-              name: "Breach Protocol",
-              perks: [
-                {
-                  name: "Total Recall",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/total-recall-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Datamine Mastermind",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/datamine-mastermind-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Quickhacks",
-              perks: [
-                {
-                  name: "Critical Error",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/critical-error-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 1,
-                  value: 0,
-                },
-                {
-                  name: "Plague",
-                  requirement: "",
-                  effects: [],
-                  imgUrl:
-                    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/plauge-perk-cyberpunk-2077-wiki-guide.png",
-                  maxValue: 3,
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
-        {
-          name: "Cool",
-          info: "",
-          img:
-            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/cool-attribute-cyberpunk-2077-wiki-guide.png",
-          value: 0,
-          perkTrees: [
-            {
-              name: "Stealth",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-            {
-              name: "Cold Blood",
-              perks: [
-                {
-                  name: "perk1",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-                {
-                  name: "perk2",
-                  requirement: "",
-                  effects: [],
-                  imgUrl: "",
-                  maxValue: 2,
-                  value: 0,
-                },
-              ],
-            },
-          ],
-        },
+        coolStats,
+        intelligenceStats,
+        technicalAbilityStats,
+        reflexesStats,
+        bodyStat,
       ],
     };
   },
+  watch: {
+    allocatedPoints: {
+      immediate: false,
+      deep: true,
+      handler(value) {
+        const stringifiedBuild = JSON.stringify(value);
+        var newurl = `${window.location.protocol}//${window.location.host}${window.location.pathname}?saved=${stringifiedBuild}`;
+        window.history.pushState({ path: newurl }, "", newurl);
+      },
+    },
+  },
+  mounted() {
+    const params = new URLSearchParams(window.location.search);
+    this.allocatedPoints = JSON.parse(params.get("saved"));
+  },
+  methods: {},
 };
 </script>
 
---
+++ src/data.js
@@ -0,0 +1,353 @@
+export const coolStats = {
+  name: "Cool",
+  info: "",
+  img:
+    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/cool-attribute-cyberpunk-2077-wiki-guide.png",
+  perkTrees: [
+    {
+      name: "Stealth",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+    {
+      name: "Cold Blood",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+  ],
+};
+export const intelligenceStats = {
+  name: "Intelligence",
+  info: "",
+  img:
+    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/intelligence-attribute-cyberpunk-2077-wiki-guide.png",
+  perkTrees: [
+    {
+      name: "Breach Protocol",
+      perks: [
+        {
+          name: "Total Recall",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/total-recall-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Datamine Mastermind",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/datamine-mastermind-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 2,
+        },
+      ],
+    },
+    {
+      name: "Quickhacks",
+      perks: [
+        {
+          name: "Critical Error",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/critical-error-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Plague",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/plauge-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+      ],
+    },
+  ],
+};
+
+export const technicalAbilityStats = {
+  name: "Technical Ability",
+  info: "",
+  img:
+    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/technical-ability-attribute-cyberpunk-2077-wiki-guide.png",
+  perkTrees: [
+    {
+      name: "Crafting",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+    {
+      name: "Engineering",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+  ],
+};
+
+export const reflexesStats = {
+  name: "Reflexes",
+  info: "",
+  img:
+    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/reflexes-attribute-cyberpunk-2077-wiki-guide.png",
+  perkTrees: [
+    {
+      name: "Blades",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+    {
+      name: "Handguns",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+    {
+      name: "Assault",
+      perks: [
+        {
+          name: "perk1",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+        {
+          name: "perk2",
+          requirement: "",
+          effects: [],
+          imgUrl: "",
+          maxValue: 2,
+        },
+      ],
+    },
+  ],
+};
+
+export const bodyStat = {
+  name: "Body",
+  info: "",
+  img:
+    "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/body-attribute-cyberpunk-2077-wiki-guide.png",
+  perkTrees: [
+    {
+      name: "Athletics",
+      perks: [
+        {
+          name: "Gladiator",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/gladiator-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 2,
+        },
+        {
+          name: "Pack Mule",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/pack-mula-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Epimorphisis",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/edimorphosis-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+        {
+          name: "Like a Butterfly",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/like-a-butterfly-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Dog of War",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/dog-of-war-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 2,
+        },
+        {
+          name: "Marathoner",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/marathoner-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Multitasker",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/multitasker-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Steel and Chrome",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/steel-and-chrome-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 2,
+        },
+        {
+          name: "Divided Attention",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/divided-attention-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+        {
+          name: "Invincible",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/invincible-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+        {
+          name: "Soft on your feet",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/soft-on-your-feet-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+      ],
+    },
+    {
+      name: "Annihilation",
+      perks: [
+        {
+          name: "Pump it, louder!",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/pump-it-louder-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 2,
+        },
+        {
+          name: "Momentum Shift",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/momentum-shift-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+      ],
+    },
+    {
+      name: "Street Brawler",
+      perks: [
+        {
+          name: "Crushing Blows",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/crushing-blows-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 3,
+        },
+        {
+          name: "Opportune Strike",
+          requirement: "",
+          effects: [],
+          imgUrl:
+            "https://cyberpunk2077.wiki.fextralife.com/file/Cyberpunk-2077/opportune-strike-perk-cyberpunk-2077-wiki-guide.png",
+          maxValue: 1,
+        },
+      ],
+    },
+  ],
+};
